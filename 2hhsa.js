8d7ee3b4ad33c77df0b888996895be2acae79797
timurumarov0404-crypto <timurumarov0404@mail.ru>
2026-04-07 23:18:44 +0400

    Update 2hhsa.js

--- 2hhsa.js
+++ 2hhsa.js
@@ -24,3 +24,24 @@ function initIncreaseNumberAnimation() {
 }
 
 initIncreaseNumberAnimation();
+
+
+document.querySelector( "#budget").addEventListener('change', function handleSelectChange(event) {
+  let otherInput = document.querySelector(".form__other-input")
+  if (event.target.value === 'other') {
+    let formContainer =document.createElement("div")
+    formContainer.classList.add("form__group")
+    formContainer.classList.add("form__other-input")
+    let input = document.createElement("input")
+    input.placeholder=("Введите ваш вариант")
+    input.type=("number")
+    formContainer.appendChild(input)
+    let form = document.querySelector(".form form")
+    let button = document.querySelector(".form__submit");
+    form.insertBefore(formContainer,button)
+  }
+  if (event.target.value !== 'other' && Boolean(otherInput)) {
+   form = document.querySelector(".form form")
+   form.removeChild(otherInput)
+  }
+});
